a9138174e49c7968746e9158d6eabf85c26f6af9
Jason Di Bartolo <jasondibartolo98@gmail.com>
2023-01-02 18:01:27 +0100

    Initial Commit

---
+++ src/main/kotlin/it/develhope/exercise2/Start.kt
@@ -0,0 +1,10 @@
+package it.develhope.exercise2
+
+fun main(){
+    val number: Short = 1234
+    val number1: Long = 4321
+    val number2 = 1111.0f
+    val sum : Float = number + number1 + number2
+
+    println("sum : $sum")
+}
